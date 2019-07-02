331bbedb41c948cd8eaea650bb198491e3f10c1c
David Waller <dwaller@apple.com>
2019-07-02 07:27:51 +0200

    mannequin editor tester story

--- src/components/MannequinEditor/MannequinEditor.stories.tsx
+++ src/components/MannequinEditor/MannequinEditor.stories.tsx
@@ -1,16 +1,26 @@
 import { storiesOf } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { MannequinEditor } from './index'
 import { heroToMannequin } from '../../utils'
 import { heroList } from '../../data'
+import { Mannequin } from '../../types'
 
 const defaultMannequin = heroToMannequin(heroList[0])
 
-storiesOf('MannequinEditor', module).add('basic', () => {
-  return (
-    <MannequinEditor
-      set={mqn => console.log(mqn)}
-      mannequin={defaultMannequin}
-    />
-  )
-})
+storiesOf('MannequinEditor', module)
+  .add('static', () => {
+    return (
+      <MannequinEditor
+        set={mqn => console.log(mqn)}
+        mannequin={defaultMannequin}
+      />
+    )
+  })
+  .add('tester', () => {
+    return <Tester />
+  })
+
+const Tester: React.FunctionComponent = () => {
+  const [mannequin, setMannequin] = useState<Mannequin>(defaultMannequin)
+  return <MannequinEditor set={setMannequin} mannequin={mannequin} />
+}
